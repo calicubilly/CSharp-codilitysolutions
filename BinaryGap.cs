ea3e668ed81218a282c45e3cfed1e4f1d5de2edd
Arvin <repolarvin@gmail.com>
2019-04-06 10:16:30 +0800

    binary gap

---
+++ BinaryGap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Test
+{
+    public class BinaryGap
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(2147483647));
+            Console.WriteLine(Solution(529));
+            Console.WriteLine(Solution(1041));
+            Console.WriteLine(Solution(1));
+            Console.WriteLine(Solution(3));
+            Console.WriteLine(Solution(5));
+            Console.WriteLine(Solution(32));
+        }
+
+        static int Solution(int N)
+        {
+            int Longest = 0, ZeroCtr = 0;
+
+            foreach (char c in Convert.ToString(N, 2))
+            {
+                if (c == '0')
+                    ZeroCtr++;
+                else
+                {
+                    if (ZeroCtr > Longest)
+                        Longest = ZeroCtr;
+                    ZeroCtr = 0;
+                }
+            }
+
+            return Longest;
+        }
+
+    }
+}
